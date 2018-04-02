3a5c39fad0b56bd0e143ad12047592221557c768
HarshITBizRuntime <harsh.r@bizruntime.net>
2018-04-02 07:42:17 -0700

    Sample Rocket MQ Application

---
+++ RocketMQDemo/src/com/lara/Producer.java
@@ -0,0 +1,31 @@
+
+package com.lara;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+public class Producer 
+{
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        producer.start();
+
+        for (int i = 0; i < 1000; i++) 
+        {
+            try 
+            {
+                Message msg = new Message("TopicTest" /* Topic */,"TagA" /* Tag */,("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */);
+                SendResult sendResult = producer.send(msg);
+                System.out.printf("%s%n", sendResult);
+            } 
+            catch (Exception e) 
+            {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
+        producer.shutdown();
+    }
+}
